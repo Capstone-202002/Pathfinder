a2506eab091c5d88db9bad13b26f8a961b3f61b8
RE-A <skyseven73@gmail.com>
2020-09-18 16:18:44 +0900

    #0 빌드 터지는거 고침

--- public/electron.js
+++ public/electron.js
@@ -12,10 +12,18 @@ const createWindow = () => {
         width: 350,
         height: 600,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false
         }
     })
-    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
+    if (isDev) {
+        // 개발 중에는 개발 도구에서 호스팅하는 주소에서 로드
+        mainWindow.loadURL('http://localhost:3000');
+        mainWindow.webContents.openDevTools();
+    } else {
+        // 프로덕션 환경에서는 패키지 내부 리소스에 접근
+        mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+    }
+
 
     if (isDev) {
         // Open the DevTools.
