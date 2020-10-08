a08dc243c2e8284d068157d8f4debf449de3bd34
kze <esimileiar81@gmail.com>
2020-10-08 21:32:25 +0900

    UI#01 Mainframe Added

---
+++ src/Component/UI/Mainframe/Mainframe.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Box from '@material-ui/core/Box';
+import {sizing} from '@material-ui/system';
+import {makeStyles, useTheme} from '@material-ui/core/styles';
+import { CssBaseline } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    spacing : 10,
+    //application itself
+    root: {
+        display: 'flex',
+        verticalAlign: 'middle',
+        flexDirection: 'column',
+        width : 1366,
+        height : 768,
+        minWidth: 940,
+        minHeight: 600,
+    },
+    //except system message
+    app:{
+        display: 'flex',
+        width: '100%',
+        height:'calc(100% - 20px)'
+    },
+    //App's Left Side
+    menu: {
+        width : 250,
+        minHeight:500,
+        marginBottom: 110                
+    },
+    //App's Right Side
+    right: {
+        display: 'flex',
+        flexDirection: 'column',
+        width : 'calc(100% - 250px)',
+        minWidth : 690,
+        minHeight : 580
+    },
+    //Top title Area
+    title: {
+        height: 60,
+        width : "100%" 
+    },
+    //Main function area
+    display: {
+        width : "100%",
+        height : "100%"
+    },
+    //System Message area
+    message:{
+        width : "100%",
+        height : 20
+    }
+}))
+
+export default function Mainframe(props){
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    return(
+        <div className={classes.root}>
+            <CssBaseline />
+            <div className={classes.app}>
+                {/* application space*/}
+
+                <div className={classes.menu}>
+                    {/*App's Left Menu Section*/}
+
+                </div>
+                <div className={classes.right}>
+                    {/*App's Right Display Section*/}
+                    <div className={classes.title}>
+                        {/*title bar section*/}
+
+                    </div>
+                    <div className={classes.display}>
+                        {/*App's Main function display Section*/}
+                        {props.contents}
+                    </div>
+
+
+                </div>
+            </div>
+            <div className={classes.message}>
+                {/*system message space*/}
+            </div>
+        </div>
+    );
+}
