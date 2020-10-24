88d9ed3b5c04f90a822da4cf1f0aab8a24a9dc4e
kze <esimileiar81@gmail.com>
2020-10-24 23:55:49 +0900

    #19 LeftMenu

--- src/Component/UI/Mainframe/LeftMenu.jsx
+++ src/Component/UI/Mainframe/LeftMenu.jsx
@@ -6,12 +6,14 @@ import Paper from '@material-ui/core/Paper'
 import { Typography,AccordionDetails, AccordionSummary } from "@material-ui/core";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import InsertEmoticon  from "@material-ui/icons/InsertEmoticon";
+import { AnimateSharedLayout, motion } from "framer-motion";
 
 const menuTexts = ['디렉토리 뷰', '다운로드 어시스트', '자동 폴더 정리', '설정']
 const useStyles = makeStyles((theme) => ({
     iconSection:{
         display: "flex",
         flexDirection: "column",
+        justifyContent: "center",
         width : '100%',
         height : "220px",
         alignItems : "center",
@@ -31,21 +33,46 @@ const useStyles = makeStyles((theme) => ({
         display : 'flex',
         flexDirection: 'column',
         height : "240px",
-        width : "100%"
+        width : "100%",
+        margin : '0',
+        padding : '0'
     },
     menuButtons:{
-        //display : 'flex',
+        display : 'flex',
+        flexDirection : 'row',
         width : '100%',
         height : '60px',
+        justifyContent:'center',
+        alignItems : 'center',
         //paddingLeft : '25px',
         //verticalAlign : 'center',
         textAlign : 'left',
-        float : 'left'
+        backgroundColor  : 'red'
+    },
+    insideButton:{
+        display  : 'flex',
+        flexDirection : 'row',
+        justifyContent :'flex-start',
+        alignItems : 'center',
+        width : '100%',
+        height : '100%',
+    },
+    buttonTextStyle:{
+        position : 'absolute',
+        x : '40px',
+        paddingLeft : '40px'
+    },
+    buttonClickCycleAnimation:{
+        position : 'relative',
+        x : '20px'
     }
 }))
+
 export default function LeftMenu(props){
     const classes = useStyles();
     const theme = useTheme();
+    const [selected, setSelected] = useState(null);
+
     return(
         <>
             <div>
@@ -63,30 +90,79 @@ export default function LeftMenu(props){
 
                     </AccordionDetails>
                 </Accordion>
+                <AnimateSharedLayout>
+                    <ul className={classes.menuButtonsSection}>
+                        {menu.map(item => (
+                            <Item
+                                key={item}
+                                isSelected={selected === item}
+                                onClick={() => setSelected(item)}
+                                menuItem = {menuTexts[item]}
+                            />
+                        ))}
+                    </ul>
+                </AnimateSharedLayout>
             </div>
-            <div id='directory_view' className={classes.menuButtonsSection}>
-                <div className={classes.menuButtons}>
-                    <Button className={classes.menuButtons}>
-                        {menuTexts[0]}
-                    </Button>
-                </div>
-                <div id='download_assist' className={classes.menuButtons}>
-                    <Button className={classes.menuButtons}>
-                        {menuTexts[1]}
-                    </Button>
-                </div>
-                <div id='applied_sortistics' className={classes.menuButtons}>
-                    <Button className={classes.menuButtons}>
-                        {menuTexts[2]}
-                    </Button>
-                </div>
-                <div id='settings' className={classes.menuButtons}>
-                    <Button className={classes.menuButtons}>
-                        {menuTexts[3]}
-                    </Button>
-                </div>
-            </div>
+            
+            
         </>
 
     );
+}
+
+const menu = [0,1,2,3];
+
+function Item({ isSelected, onClick, menuItem}){
+    const classes = useStyles();
+    return (
+        <>
+            <li className={classes.menuButtons} onClick={onClick} >
+                    <motion.div
+                        className = {classes.insideButton}
+                        whileHover={{scale:1.1}}
+                        whileTap={{scale:0.9}}
+                    >
+                        {
+                        isSelected && (
+                            /*
+                            <motion.div
+                                className={classes.menuButtons}
+                                initial={false}
+                                animate={{backgroundColor:'#ffffff'}}
+                                transition={spring}
+                            >
+
+                            </motion.div>*/
+                            <motion.div
+                                className={classes.buttonClickCycleAnimation}
+                                style={{backgroundColor : '#ffffff', width : '20px', height : '20px'}}
+                                animate={{
+                                    scale: [1, 2, 2, 1, 1],
+                                    rotate: [0, 0, 270, 270, 0],
+                                    borderRadius: ["20%", "20%", "50%", "50%", "20%"]
+                                }}
+                                transition={{
+                                    duration: 2,
+                                    ease: "easeInOut",
+                                    times: [0, 0.2, 0.5, 0.8, 1],
+                                    repeatDelay: 1
+                                }}
+                            />
+                        )
+                        }
+                        <Typography className={classes.buttonTextStyle}>
+                            {menuItem}
+                        </Typography>
+                    </motion.div>
+                
+
+            </li>
+        </>
+    );
+}
+
+const spring = {
+    type: "spring",
+    stiffness: 100,
+    damping : 30
 }
